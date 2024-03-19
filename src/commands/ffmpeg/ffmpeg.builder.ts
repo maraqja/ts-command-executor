7cdd52ae336462e02b67e6efd9090dee1dce9d5c
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-03-19 21:43:46 +0300

    use builder pattern for building ffmpeg command

---
+++ src/commands/ffmpeg/ffmpeg.builder.ts
@@ -0,0 +1,30 @@
+export class FfmpegBuilder {
+    private inputPath!: string;
+    private options: Map<string, string> = new Map();
+    constructor() {
+        this.options.set('-c:v', 'libx264');
+    }
+
+    setInput(inputPath: string): this {
+        this.inputPath = inputPath;
+        return this;
+    }
+
+    setVideoSize(width: number, height: number): this {
+        this.options.set('-s', `${width}x${height}`);
+        return this;
+    }
+
+    build(outputPath: string) {
+        if (!this.inputPath) {
+            throw new Error('Не задан параметр input');
+        }
+        const args: string[] = ['-i', this.inputPath];
+        this.options.forEach((value, key) => {
+            args.push(key);
+            args.push(value);
+        });
+        args.push(outputPath);
+        return args;
+    }
+}
